22cc64e4c3df84abef7e479c8a5e7304ce7ba946
CH VAMSI <chokkapuvamsi123@gmail.com>
2023-08-17 13:48:04 +0530

    Sample MVC application for employee management

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Sample.Controllers
+{
+
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample.Models
+{
+    
+    public class Employee
+    {
+
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeDescription { get; set; }
+    }
+}
+
