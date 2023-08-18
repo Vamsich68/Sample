b9c8bcafcd62fb16b1bc8f4980ddc9748fdb893e
CH VAMSI <chokkapuvamsi123@gmail.com>
2023-08-18 17:26:42 +0530

    form

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,22 +1,22 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-
+using Sample.Models;
+using System.Diagnostics;
+//using System.Web.Mvc;
 namespace Sample.Controllers
 {
-
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index() { return View(); }
+        [HttpGet]
+        public ActionResult Edit()
         {
+            Employee employee = new Employee();
+            employee.EmployeeId = "59";
+            employee.EmployeeName ="Name";
+            employee.EmployeeDescription="Description";
+            return View( employee);
 
-            return View();
         }
-
-        public IActionResult Register()
-        {
-            return View();
-        }
-        
     }
 }
