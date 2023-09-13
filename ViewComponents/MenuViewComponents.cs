4437ed9b247e212578d7bf53107629a07cbd540c
CH VAMSI <chokkapuvamsi123@gmail.com>
2023-09-13 10:16:48 +0530

    View components

---
+++ ViewComponents/MenuViewComponents.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Sample.Repo;
+
+namespace Sample.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly IEmployeeRepo _db;
+        public MenuViewComponent(IEmployeeRepo db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var employees = _db.GetAll();
+            var len = employees.Count;
+            return View(employees);
+        }
+
+    }
+}
